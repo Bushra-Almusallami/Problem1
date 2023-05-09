a1dc2bf71f669165df9784409579e440cf0cb083
Bushra-Almusallami <130510018+Bushra-Almusallami@users.noreply.github.com>
2023-05-09 08:34:03 +0400

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -4,11 +4,11 @@ b = int(input('Enter Second number: '))
 c = int(input('Enter third number: '))
 
 
-if (a>b) and (a>c):
+if (a>=b) and (a>=c):
     print(a,"is the largest")
-elif(b>a) and (b>c):
+elif(b>=a) and (b>=c):
     print(b,"is the largest")
-elif(c>a) and (c>b):
+elif(c>=a) and (c>=b):
     print(c,"is the largest")
 
 else:
